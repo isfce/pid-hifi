0217dcb8c8c27865b3d21f4cd6fc8b765570ff85
VO <voisfce@gmail.com>
2019-10-09 19:16:29 +0200

    Ajuste Exemple testHifi main

--- Tp01_Hifi/src/main/java/exemple/TestChaineHifi.java
+++ Tp01_Hifi/src/main/java/exemple/TestChaineHifi.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 import hifi.Ampli;
 import hifi.CdPlayer;
 import hifi.ChargeurCdFactory;
+import hifi.DisqueCD;
 import hifi.IAmpli.Sources;
+import hifi.ICd;
 import hifi.IChargeur;
 import hifi.Tuner;
 
@@ -13,6 +15,8 @@ public class TestChaineHifi {
 
 	public static void main(String[] args) {
 		try {
+			//Création d'un CD
+			ICd cd1 = new DisqueCD("Titre CD1","M1,M2,M3,M4");
 			// Crée un chargeur avec la liste des disques des Beatles
 			IChargeur chargeur = ChargeurCdFactory.createChargeur("/hifi/CDBeatles.properties");
 			// Crée un Lecteur de CD et un Tuner
@@ -26,6 +30,11 @@ public class TestChaineHifi {
 			ampli.setSource(Sources.CD);
 			// Simule la leture du disque
 			lecteurCD.autoPlay();
+			//Charge le CD1
+			lecteurCD.setCd(cd1);
+			// Simule la leture du disque
+			lecteurCD.autoPlay();
+			
 			// change la source en Tuner
 			ampli.setSource(Sources.TUNER);
 			// Ajuste le volume
